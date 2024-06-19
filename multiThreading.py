f12d7e513db75d7969127123460c1ac626b6e0cb
HFarahat <farahat2312@gmail.com>
2024-06-19 15:36:27 +0300

    multiThreading using for loop and join the rest of the code after finishing !

--- multiThreading.py
+++ multiThreading.py
@@ -5,23 +5,21 @@ start = time.perf_counter()
 
 def do_something():
     print("Sleeping 1 second")
-    time.sleep(10)
+    time.sleep(1)
     print("Done sleeping ....")
 
 
-t1 = threading.Thread(target=do_something)
-t2 = threading.Thread(target=do_something)
-t3 = threading.Thread(target=do_something)
-t4 = threading.Thread(target=do_something)
+threads = []
 
-t1.start()
-t2.start()
-t3.start()
-t4.start()
+for _ in range(10):
+    t = threading.Thread(target=do_something)
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
 
 
-t1.join()
-t2.join()
 finish = time.perf_counter
 
 
