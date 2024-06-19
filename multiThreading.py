6a95a559c975b22f50609eafd03fec90911eac49
HFarahat <farahat2312@gmail.com>
2024-06-19 15:26:08 +0300

    added multi-threading using threading module

--- multiThreading.py
+++ multiThreading.py
@@ -1,16 +1,28 @@
 import time
+import threading
 
 start = time.perf_counter()
 
 def do_something():
     print("Sleeping 1 second")
-    time.sleep(1)
+    time.sleep(10)
     print("Done sleeping ....")
 
 
-do_something()
-do_something()
-do_something()
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
+t3 = threading.Thread(target=do_something)
+t4 = threading.Thread(target=do_something)
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+
+
+t1.join()
+t2.join()
+finish = time.perf_counter
 
 
 finish = time.perf_counter()
